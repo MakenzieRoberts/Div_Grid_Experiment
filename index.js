ac2f654f58a0b4c9754456251b1cdfcd85bd1162
MakenzieRoberts <makenzie.roberts@keyin.com>
2023-06-24 19:44:30 -0230

    Update grid code copyable textarea (HTML + CSS)
    Previously, the textarea containing the grid HTML contained HTML with inline styling. When tested, it was a mess to copy+paste due to the sheer volume of text (When I tried to paste all that HTML with inline styling into a new project in VSCode, it's format-on-save feature and the Prettier extension couldn't handle the amount of text, and either took up to a minute to process or just straight-up crashed.)
    
    This update removes the inline styling from the HTML and instead adds another textarea for styling with CSS - so now the user can copy both the provided HTML and CSS into their project and get the same results as before, but in a much better way.

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ window.addEventListener("DOMContentLoaded", async (event) => {
 		applyExtractedColoursToGrid(imageData.channels);
 
 		addCopyableHtmlToTextarea();
-
+		addCopyableCssToTextarea();
 		unhideResultHeadersAndTextarea();
 
 		const container = document.querySelector("#grid");
@@ -94,7 +94,12 @@ function removeGridCellLabels() {
 
 /* ———————————————————————————— addCopyableHtmlToTextarea() ——————————————————————————— */
 
-function addCopyableHtmlToTextarea() {
+/* Old function that created HTML with inline styling so the user can copy it into
+their project. There are so many divs and so much HTML though that adding the
+inline styling literally makes vscode nearly crash on format because it's so much
+text. So in the new function below I'm removing all the inline styling from the
+html so it and the CSS can be completely separate. */
+function OldAddCopyableHtmlToTextareaWithInlineStyling() {
 	/* Get grid html as a string and add it to the text area so the user can copy it */
 	let grid = document.getElementById("grid");
 
@@ -113,16 +118,197 @@ function addCopyableHtmlToTextarea() {
 		/class="row" style="/g,
 		`class="row" style="display: grid; height: fit-content; width: fit-content; `
 	);
-	let textArea = document.getElementById("textarea");
+	let textArea = document.getElementById("html-textarea");
 
 	textArea.value = gridHTML;
 }
 
+/* New version of the function that has inline styling removed. Inline styling for
+so many divs is a complete mess, so I'm separating it into HTML and CSS for the
+user to copy. */
+function addCopyableHtmlToTextarea() {
+	/* Get grid html as a string and add it to the text area so the user can copy it */
+	let grid = document.getElementById("grid");
+	let gridClone = grid.cloneNode(true);
+
+	/* I'm grabbing the HTML from a clone of the actual grid HTML nodes used to
+	display the grid to the user. Removing all inline styling that was there to
+	properly display the grid. 
+
+	As I'm starting to separate the HTML from the CSS, I'm starting to realize
+	that I should probably go back to the grid generation function(s) and remove
+	all inline styling from my own grid display as well and just put it in my CSS,
+	maybe with the use of dynamically generating class names for each colour and
+	adding each colour class to their respective cell HTML. 
+
+	I should have done that in the beginning, and it was definitely a bad
+	design decision on my part, but at first this project was just a proof of
+	concept so I didn't worry about it. !REMEMBER - From now on, even for small experimental
+	projects, I'll make an effort to design my code in a way that is scalable and
+	efficient. */
+
+	console.log(gridClone.children);
+
+	for (node of gridClone.children) {
+		console.log(node);
+		node.removeAttribute("style");
+		if (node.hasChildNodes()) {
+			for (child of node.children) {
+				child.removeAttribute("style");
+			}
+		}
+	}
+
+	let gridHTML = gridClone.innerHTML;
+
+	/* Formatting HTML text (line breaks, indents) (more readable and makes user's
+	copy/paste easier and neater) */
+
+	/* Adds new line after parent row elements */
+	gridHTML = gridHTML.replace(/"row">/g, `"row">\n`);
+	/* Adds new line after div end tags */
+	gridHTML = gridHTML.replace(/<\/div>/g, `<\/div>\n`);
+	/* Indents cell class divs (children of row class divs)  */
+	gridHTML = gridHTML.replace(/<div class="cell/g, `    <div class="cell`);
+
+	let textArea = document.getElementById("html-textarea");
+
+	textArea.value = gridHTML;
+}
+
+function addCopyableCssToTextarea() {
+	/* My goal is to list the CSS in an efficient way, so if more than one cell has the same colour I want them listed with commas like: 
+		#c1-r1, #c2-r2{
+			background-colour: rgb(0,0,0,0);
+		}  
+	*/
+	/* Get grid html as a string and add it to the text area so the user can copy it */
+	let gridNodeList = document.getElementById("grid");
+
+	let gridHTML = gridNodeList.innerHTML;
+	// console.log("gridHTML: ", gridHTML);
+
+	// console.log("grid: ", gridNodeList);
+	// console.log("grid.children: ", gridNodeList.children);
+	// console.log("grid.children[0]: ", gridNodeList.children[0]);
+	// console.log("grid.children[0].id: ", gridNodeList.children[0].id);
+	// console.log(
+	// 	"grid.children[0].className: ",
+	// 	gridNodeList.children[0].className
+	// );
+	// console.log(
+	// 	"grid.children[0].style: ",
+	// 	gridNodeList.children[0].style.cssText
+	// );
+
+	// console.log("gridnodelist childnodes: ", gridNodeList.childNodes);
+	let gridWrapperNodes = document.getElementById("grid-wrapper").childNodes;
+	// console.log("gridWrapperNodes: ", gridWrapperNodes);
+	/* ———————————————————————————————————————————————————————————————————————————— */
+	let rowNode = document.querySelector(".row");
+	// console.log("rowNode: ", rowNode.style.cssText);
+
+	/* General row styling */
+	let rowStyle = rowNode.style.cssText;
+	let rowClassCssString = `.row{display: grid; height: fit-content; width: fit-content; ${rowStyle}}`;
+
+	/* General cell styling */
+	let cellClassCssString = `.cell{height: ${cellHeight}px; width: ${cellWidth}px; border: none;}`;
+
+	/* Specific individual cell styling (actual cell colours making up the image)*/
+
+	let cellNodes = document.querySelectorAll(".cell");
+	// console.log("cellNodes: ", cellNodes);
+	// console.log("cellNodes length: ", cellNodes.length);
+
+	let cellIdBackgroundColours = {};
+	for (let i = 0; i < cellNodes.length; i++) {
+		// console.log("cellNodes[i]: ", cellNodes[i]);
+		// console.log("cellNodes[i].id: ", cellNodes[i].id);
+		// console.log("cellNodes[i].style: ", cellNodes[i].style.cssText);
+		// console.log("cellNodes[i].style: ", cellNodes[i].style.backgroundColor);
+		let cellId = cellNodes[i].id;
+
+		cellIdBackgroundColours["#" + cellId] = cellNodes[i].style.backgroundColor;
+	}
+
+	// console.log("cellIdBackgroundColours: ", cellIdBackgroundColours);
+
+	// Create a new object to store the duplicate values
+	const duplicateValues = {};
+
+	// Iterate over the original object
+	for (const key in cellIdBackgroundColours) {
+		const value = cellIdBackgroundColours[key];
+
+		// Check if the value already exists in the duplicateValues object
+		if (duplicateValues[value]) {
+			duplicateValues[value].push(key); // Add the key to the array
+		} else {
+			duplicateValues[value] = [key]; // Create a new array with the key
+		}
+	}
+
+	// console.log(duplicateValues);
+
+	let cellIdsBackgroundColourCssStrings = [];
+	// Iterate over the duplicateValues object
+	for (const key in duplicateValues) {
+		const value = duplicateValues[key];
+		let cssString = `${value}{background-color: ${key};}\n\n`;
+		// console.log("cssString: ", cssString);
+		/* !TODO: Now I just need to store this and format the whole CSS string to place into the textarea */
+		cellIdsBackgroundColourCssStrings.push(cssString);
+	}
+
+	let completeCssString = `${rowClassCssString}\n\n${cellClassCssString}\n\n${cellIdsBackgroundColourCssStrings.join(
+		""
+	)}`;
+
+	// console.log("completeCssString: ", completeCssString);
+
+	/* !TODO: Maybe instead of having hundreds of css selectors due to the cell
+	ids, I could add a class to each cell that represents the rgba values, eg.
+	class="rgba-56-27-9" or something that can easily convert to a usable rgba
+	value format. */
+
+	/* !TODO: I'm noticing the CSS strings that are fully opaque leave out the
+	alpha value instead of setting it to 1. So some cell ids only have 3 values
+	(aka rgb not rgba). Just noting this in case an issue comes up later, but for
+	now it seems to be working fine. */
+
+	/* ———————————————————————————————————————————————————————————————————————————— */
+	// for (let i = 0; i < gridWrapperNodes.length; i++) {
+	// 	console.log("TEST", i);
+	// 	console.log("gridNodeList[i]: ", gridWrapperNodes[i]);
+	// 	console.log("gridNodeList[i].id: ", gridWrapperNodes[i].id);
+	// 	console.log("gridNodeList[i].className: ", gridWrapperNodes[i].className);
+	// 	console.log("gridNodeList[i].style: ", gridWrapperNodes[i].style.cssText);
+	// }
+	/* For formatting I'm just editing the grid html text string for now, I'll do this in
+	a neater way later. !TODO */
+
+	// /* Formatting HTML text (line breaks, indents) (more readable and makes user's
+	// copy/paste easier and neater) */
+	// gridHTML = gridHTML.replace(/(\);\">)/g, `);">\n`);
+	// gridHTML = gridHTML.replace(/<\/div>/g, `<\/div>\n`);
+	// gridHTML = gridHTML.replace(/<div class="cell/g, `\t<div class="cell`);
+
+	// /* Adding all necessary inline styling */
+	// gridHTML = gridHTML.replace(
+	// 	/class="row" style="/g,
+	// 	`class="row" style="display: grid; height: fit-content; width: fit-content; `
+	// );
+
+	let textArea = document.getElementById("css-textarea");
+
+	textArea.value = completeCssString;
+}
 /* ————————————————————————— unhideResultHeadersAndTextarea() ————————————————————————— */
 
 function unhideResultHeadersAndTextarea() {
 	for (let resultHeader of resultHeaders) {
-		console.log(resultHeader);
+		// console.log(resultHeader);
 		resultHeader.hidden = false;
 		resultHeader.classList.remove("collapsed");
 	}
